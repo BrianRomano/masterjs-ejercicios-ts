d8b56419251757c2df182a46c7b1b72077880b78
Brian Romano <brianromanook@gmail.com>
2020-11-04 16:00:29 -0300

    Pruebas

--- js/01-clase-json.js
+++ js/01-clase-json.js
@@ -1,11 +1,11 @@
 var bicicleta = {
-    color: 'Rojo', 
-    modelo: 'BMX', 
-    frenos: 'de disco', 
-    velocidadMaxima: '60km', 
-    cambiaColor: function(color){
-        this.color = color; 
-        console.log(this);
-    }
-}
-bicicleta.cambiaColor("Azul")
+  color: "Rojo",
+  modelo: "BMX",
+  frenos: "de disco",
+  velocidadMaxima: "60km",
+  cambiaColor: function (color) {
+    this.color = color;
+    console.log(this);
+  },
+};
+bicicleta.cambiaColor("Azul");
